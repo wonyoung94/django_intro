1766be28cb81aea46986a08e95efd7c8bb150243
wonyoung94 <jun2young94@gmail.com>
2023-10-31 16:11:15 +0900

    hell

--- app_intro/views.py
+++ app_intro/views.py
@@ -1,7 +1,71 @@
 from django.shortcuts import render
+import random
+from faker import Faker
 
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
 
+
+def hello(request):
+    username = '전원영'
+
+    result = {
+        'username': username,
+    }
+
+    return render(request, 'hello.html',result)
+
+
+def dinner(request):
+    meuns=['라면', '김밥', '떡볶이']
+    pick = random.choice(meuns)
+
+    result = {
+        'pick': pick,
+    }
+
+    return render(request, 'dinner.html', result)
+
+def lotto(request):
+    numbers = range(1,46)
+    win = random.sample(numbers, 6)
+
+    result = {
+        'win': win,
+    }
+
+    return render(request, 'lotto.html', result)
+
+
+def greeting(request, name):
+
+    result = {
+        'name':name,
+    }
+
+    return render(request, 'greeting.html', result)
+    
+def cube(request, num):
+
+    result = {
+        'num': num,
+        'cube': num ** 3,
+    }
+
+    return render(request, 'cube.html', result)
+
+def posts(request):
+    posts = []
+
+    fake = Faker()
+
+    for i in range(100):
+        posts.append(fake.text())
+
+    result = {
+        'posts': posts,
+    }
+
+    return render(request, 'post.html', result)
